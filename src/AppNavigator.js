e3829e09f7c121e117f53a39afa7b3ee13e04038
Fran Villalba Segarra <franvillalba@noreply.com>
2019-05-27 17:00:51 +0200

    Update references

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -3,15 +3,11 @@ import { createStackNavigator } from "react-navigation";
 
 import Home from "./screens/Home";
 import Auth from "./screens/Auth";
-import Storage from "./screens/Storage";
-import SubscriptionDetails from "./screens/Storage/SubscriptionDetails";
 import CreateFolder from "./screens/CreateFolder";
 
 const routeConfig = {
   Home: { screen: Home },
   Auth: { screen: Auth },
-  Storage: { screen: Storage },
-  SubscriptionDetails: { screen: SubscriptionDetails },
   CreateFolder: { screen: CreateFolder }
 };
 
