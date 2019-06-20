76236f49455443f5379b1a73cd14411e0e368d70
JGonzalez <jesusjuangonzalez@gmail.com>
2019-06-20 17:11:12 +0200

    Cleaning generated files

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package host.exp.exponent.generated;
-
-// This file is auto-generated. Please don't rename!
-public class DetachBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "exp0d6220867549490f86b82b4152b680fc://192.168.1.145:19000";
-
-}
