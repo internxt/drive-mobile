cffde36e9d2a822b3d5ec3a25f4b8a1581258839
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-11-27 22:31:17 +0100

    load fonts

--- App.tsx
+++ App.tsx
@@ -1,18 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { ActivityIndicator, processColor, StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux'
 import { store } from './src/store'
+import { loadFonts } from './src/helpers'
 
 export default function App() {
-  return (
-    <Provider store={store}>
+  const [appInitialized, setAppInitialized] = useState(false);
+
+  Promise.all([
+    loadFonts()
+  ]).then(() => {
+    setAppInitialized(true);
+  })
+
+  return <Provider store={store}>
+    {appInitialized ?
       <View style={styles.container}>
         <Text>Open up App.tsx to start working on your app!</Text>
         <StatusBar style="auto" />
       </View>
-    </Provider>
-  );
+      : <View style={styles.container}>
+        <ActivityIndicator color={'#00f'} />
+      </View>
+    }
+  </Provider>
+    ;
 }
 
 const styles = StyleSheet.create({
