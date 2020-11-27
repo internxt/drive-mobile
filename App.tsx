ed45efc932e3c456f629eafb91298a88891ff565
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-11-27 22:12:47 +0100

    redux migration to ts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import { store } from './src/store'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
