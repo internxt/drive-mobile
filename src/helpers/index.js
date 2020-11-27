5ccfe9e2f94b18305e82703cf5b1006c0d8daadc
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-11-27 21:27:46 +0100

    clean folder

--- src/helpers/index.js
+++
@@ -1,6 +0,0 @@
-export * from './getIcon';
-export * from './loadFonts';
-export * from './deviceStorage';
-export * from './utils';
-export * from './inxt';
-export * from './getHeaders';
--- src/styles/base.js
+++
@@ -1,5 +0,0 @@
-import { Dimensions } from 'react-native';
-
-export const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get(
-  'window'
-);
