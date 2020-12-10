0b368984d468e9ba71dacd989c8bb05849736d6e
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-12-10 14:07:29 +0100

    Migration to SDK40

--- android/app/src/main/java/com/internxt/cloud/generated/BasePackageList.java
+++ android/app/src/main/java/com/internxt/cloud/generated/BasePackageList.java
@@ -7,6 +7,7 @@ import org.unimodules.core.interfaces.Package;
 public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
+        new expo.modules.application.ApplicationPackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.documentpicker.DocumentPickerPackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
@@ -19,6 +20,7 @@ public class BasePackageList {
         new expo.modules.localauthentication.LocalAuthenticationPackage(),
         new expo.modules.location.LocationPackage(),
         new expo.modules.permissions.PermissionsPackage(),
+        new expo.modules.securestore.SecureStorePackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.sqlite.SQLitePackage(),
         new expo.modules.updates.UpdatesPackage()
