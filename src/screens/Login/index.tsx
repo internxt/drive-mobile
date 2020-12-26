07527ded685a801bfa7f7c9f05522c36d9482bc1
demidi <demidietsinf@gmail.com>
2020-12-26 19:37:37 +0100

    change detail for merge master

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -34,7 +34,7 @@ function Login(props: LoginProps) {
   useEffect(() => {
     if (props.authenticationState.loggedIn === true) {
       const rootFolderId = props.authenticationState.user.root_folder_id;
-      props.navigation.replace('Biometric', {
+      props.navigation.replace('FileExplorer', {
         folderId: rootFolderId
       })
     } else {
