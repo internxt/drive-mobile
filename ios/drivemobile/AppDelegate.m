51fc72a093ff17815adfda396ce21a3791e8be6e
Aldimir <aldimirprincipal@gmail.com>
2021-01-14 16:26:00 +0100

    iOS deeplinking working

--- ios/drivemobile/AppDelegate.m
+++ ios/drivemobile/AppDelegate.m
@@ -60,6 +60,21 @@ static void InitializeFlipper(UIApplication *application) {
   return YES;
 }
 
+- (BOOL)application:(UIApplication *)application
+openURL:(NSURL *)url
+options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+return [RCTLinkingManager application:application openURL:url options:options];
+}
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+return [RCTLinkingManager application:application
+continueUserActivity:userActivity
+restorationHandler:restorationHandler];
+}
+
 - (RCTBridge *)initializeReactNativeApp
 {
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:self.launchOptions];
