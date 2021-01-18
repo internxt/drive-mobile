1c5e498b4f23b868fb5a17d7456cdbc8640b557b
Aldimir <aldimirprincipal@gmail.com>
2021-01-18 17:29:07 +0100

    Android config for share to intent

--- android/app/src/main/java/com/internxt/cloud/MainActivity.java
+++ android/app/src/main/java/com/internxt/cloud/MainActivity.java
@@ -1,8 +1,12 @@
 package com.internxt.cloud;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -17,7 +21,7 @@ import expo.modules.splashscreen.SplashScreenImageResizeMode;
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+      super.onCreate(savedInstanceState);
     // SplashScreen.show(...) has to be called after super.onCreate(...)
     // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
     SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, ReactRootView.class, false);
@@ -41,10 +45,20 @@ public class MainActivity extends ReactActivity {
             protected Bundle getLaunchOptions() {
 
                 Intent intent = MainActivity.this.getIntent();
+                String name = null;
                 Bundle bundle = new Bundle();
                 Uri uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
                 if (uri != null) {
-                    bundle.putString("fileUri", uri.toString());
+                    Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+                    try {
+                        if (cursor != null && cursor.moveToFirst()) {
+                            name = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                            bundle.putString("fileUri", uri.toString());
+                            bundle.putString("fileName", name);
+                        }
+                    } finally {
+                        cursor.close();
+                    }
                 }else{
                     bundle.putString("fileUri", "");
                 }
