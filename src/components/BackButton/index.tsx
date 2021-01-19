8255045f759d24b307c6f4be01398587eea7fd9d
DarthegX <elgarcan@inf.upv.es>
2021-01-19 16:40:00 +0100

    back button

---
+++ src/components/BackButton/index.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { connect, useDispatch, useSelector } from 'react-redux';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableHighlight,
+  Image,
+  Pressable
+} from 'react-native';
+import { useLinkProps, useNavigation } from '@react-navigation/native';
+
+const backBtn = require("../../../assets/icons/icon-back.png");
+
+export interface BackButtonProps {
+    style?: any
+    navigation: any
+    filesState?: any
+    dispatch?: any,
+    layoutState?: any
+    authenticationState?: any
+}
+
+export function BackButton(props: BackButtonProps): JSX.Element {
+  const [iconArrowBack, setIconArrowBack] = React.useState(backBtn);
+  //const parentFolderId = useSelector(state => state.pic.folderContent.parentFolderId);
+
+  const goBack = () => {
+    //dispatch(onGetBucketContent(parentFolderId));
+    //navigation.setParams({ folderId: parentFolderId });
+    props.navigation.goBack();
+  }
+
+  return (
+    <Pressable
+      style={styles.buttonWrapper}
+      onPress={goBack}
+    >
+      <Image style={styles.icon} source={iconArrowBack} />
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonWrapper: {
+    width: 45,
+    height: 45,
+    justifyContent: 'center',
+    alignItems: 'flex-start'
+  },
+  icon: {
+    height: 18,
+    width: 11,
+    tintColor: '#0084ff'
+  }
+});
