56b61ad9d08c9d84e72d87c4812c7743c91f95ad
DarthegX <elgarcan@inf.upv.es>
2021-01-19 16:40:22 +0100

    loadfonts

--- src/helpers/loadFonts.ts
+++ src/helpers/loadFonts.ts
@@ -5,13 +5,12 @@ import * as Font from 'expo-font';
  */
 async function loadFontsAsync(): Promise<void> {
   return Font.loadAsync({
-    'CerebriSans-Bold': require('../../assets/fonts/CerebriSans-Bold.ttf'),
-    'CerebriSans-Medium': require('../../assets/fonts/CerebriSans-Medium.ttf'),
-    'CerebriSans-Regular': require('../../assets/fonts/CerebriSans-Regular.ttf'),
-    'CircularStd-Bold': require('../../assets/fonts/CircularStd-Bold.ttf'),
-    'CircularStd-Black': require('../../assets/fonts/CircularStd-Black.ttf'),
-    'CircularStd-Book': require('../../assets/fonts/CircularStd-Book.ttf'),
-    'CircularStd-Medium': require('../../assets/fonts/CircularStd-Medium.ttf')
+    'Averta-Bold': require('../../assets/fonts/Averta-Bold.otf'),
+    'Averta-Regular': require('../../assets/fonts/Averta-Regular.otf'),
+    'Averta-Extra-Bold': require('../../assets/fonts/Averta-Extra-Bold.otf'),
+    'Averta-Light': require('../../assets/fonts/Averta-Light.otf'),
+    'Averta-Black': require('../../assets/fonts/Averta-Black.otf'),
+    'Averta-Semibold': require('../../assets/fonts/Averta-Semibold.otf')
   });
 }
 
