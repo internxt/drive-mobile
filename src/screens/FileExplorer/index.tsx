cb3682465fb541c333758a6cbdda66b8b7bf6068
Aldimir <aldimirprincipal@gmail.com>
2021-02-09 14:19:27 +0100

    Activity indictaor while loading

--- src/screens/FileExplorer/index.tsx
+++ src/screens/FileExplorer/index.tsx
@@ -15,17 +15,7 @@ import ShareFilesModal from '../../modals/ShareFilesModal';
 import { Reducers } from '../../redux/reducers/reducers';
 import analytics, { getLyticsData } from '../../helpers/lytics';
 import RNFetchBlob from 'rn-fetch-blob';
-import {
-  BallIndicator,
-  BarIndicator,
-  DotIndicator,
-  MaterialIndicator,
-  PacmanIndicator,
-  PulseIndicator,
-  SkypeIndicator,
-  UIActivityIndicator,
-  WaveIndicator,
-} from 'react-native-indicators'
+import { WaveIndicator } from 'react-native-indicators'
 
 interface FileExplorerProps extends Reducers {
   navigation?: any
