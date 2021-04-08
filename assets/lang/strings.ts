4a3e89dc116d0e0792f11c24e23a9057e3fea264
Aldimir <aldimirprincipal@gmail.com>
2021-04-08 10:01:44 +0200

    Translated all texts

---
+++ assets/lang/strings.ts
@@ -0,0 +1,274 @@
+/* eslint-disable quotes */
+import LocalizedStrings from "react-native-localization"
+
+const strings = new LocalizedStrings({
+  "en_US": {
+    "screens": {
+      "login_screen": {
+        "title": "Sign in to Internxt",
+        "forgot": "Forgot your password?"
+      },
+      "register_screen": {
+        "first": "Internxt is a different cloud storage service. A better place for all your files. Welcome to the revolution. Welcome to Internxt.",
+        "second": "Files are encrypted on your device. There is no way we nor any other third_party can access them. Privacy, as it should have always been.",
+        "third": "Access Internxt from Desktop, Web or Mobile. Start using Drive today with 10 GB on us. Upgrade your storage when needed, free for a month, cancel anytime.",
+        "security_title": "Internxt Security",
+        "security_subtitle": "Internxt uses your password to encrypt and decrypt your files. Due to the secure nature of Internxt, we don't know your password. That means that if you ever forget it, your files are gone forever. With us, you're the only owner of your files. We strongly suggest you to:",
+        "suggestion_1": "Store your password. Keep it safe and secure.",
+        "suggestion_2": "Keep an offline backup of your password.",
+        "create_account_title": 'Create an account'
+      },
+      "forgot_password": {
+        "title": "Internxt security",
+        "subtitle": "As specified during the sign up process, Internxt Drive encrypts your files, and only you have access to those. We never know your password, and thus, that way, only you can decrypt your account. For that reason, if you forget your password, we can't restore your account. What we can do, however, is to delete your account and erase all its files, so that you can sign up again. Please, if you still want to procede, enter your email below so that we can process the account removal."
+      },
+      "deactivation_screen": {
+        "title": "Deactivation email",
+        "subtitle_1": "Please check your email and follow the instructions to deactivate your account so you can start using Internxt again.",
+        "subtitle_2": "Once you deactivate your account, you will be able to sign up using the same email address. Please store your password somewhere safe. With Internxt Drive, only you are the true owner of your files on the cloud. With great power there must also come great responsibility."
+      },
+      "storage": {
+        "title": "Storage",
+        "space": {
+          "title": "Storage space",
+          "used": {
+            "used": "Used",
+            "of": "of"
+          },
+          "legend": {
+            "used": "Used space",
+            "unused": "Unused space"
+          }
+        },
+        "plans": {
+          "title": "Storage plans",
+          "title-2": "Payment length",
+          "current_plan": "You are subscribed to the"
+        }
+      },
+      "photos": {
+        "screens": {
+          "photos": {
+            "albums": "Albums",
+            "all_photos": "All photos"
+          },
+          "photo_gallery": {
+            "title": "title",
+            "subtitle": "Photos"
+          }
+        }
+      }
+    },
+    "components": {
+      "buttons": {
+        "sign_in": "Sign in",
+        "create": "Create account",
+        "next": "Next",
+        "get_started": "Get started",
+        "continue": "Continue",
+        "back": "Back",
+        "creating_button": "Creating...",
+        "deactivation": "Re-send deactivation email",
+        "sing_up": "Sign up",
+        "descrypting": "Decrypting...",
+        "cancel": "Cancel",
+        "confirm": "Confirm",
+        "move": "Move",
+        "share": "Share"
+      },
+      "inputs": {
+        "email": "Email address",
+        "password": "Password",
+        "confirm_password": "Confirm password",
+        "first_name": "First name",
+        "last_name": "Last name"
+      },
+      "empty_folder": {
+        "title": "This folder is empty",
+        "subtitle": "Click the upload button to get started."
+      },
+      "app_menu": {
+        "search_box": "Search",
+        "filter": {
+          "date": "Date added",
+          "size": "Size",
+          "name": "Name",
+          "type": "File type"
+        },
+        "upload": {
+          "title": "Select type of file",
+          "documen": "Upload a document",
+          "media": "Upload media",
+          "take_photo": "Take a photo",
+          "create_folder": "Folder name"
+        },
+        "settings": {
+          "storage": "Storage",
+          "more": "More info",
+          "photos": "Photos",
+          "drive": "Drive",
+          "contact": "Contact",
+          "sign": "Sign out"
+        }
+      },
+      "file_and_folder_options": {
+        "styling": "Style color",
+        "icons": "Cover icon",
+        "type": "Type",
+        "size": "Size",
+        "move": "Move",
+        "share": "Share",
+        "delete": "Delete"
+      }
+    },
+    "modals": {
+      "move_modal": {
+        "title": "Choose a folder to move this file."
+      },
+      "share_modal": {
+        "title": "Share your Drive file with this private link and enter the number of times you would like the link to be valid:",
+        "loading": "Loading link..."
+      },
+      "delete_modal": {
+        "title": "Delete item",
+        "subtitle": "Please confirm you want to delete this item. This action can not be undone."
+      }
+    }
+  },
+  "es": {
+    "screens": {
+      "login_screen": {
+        "title": "Inicia sesión en Internxt",
+        "forgot": "¿Has olvidado la contraseña?"
+      },
+      "register_screen": {
+        "first": "Internxt es un servicio en la nube diferente. El mejor lugar para todos tus archivos. Bienvenido a la revolución. Bienvenido a Internxt.",
+        "second": "Los archivos son encriptados en tu dispositivo. Ni nosotros ni ningún servicio de terceros podemos acceder a ellos. Privacidad, como siempre tuvo que haber sido.",
+        "third": "Puedes acceder a Internxt desde la aplicación de escritorio, web o móvil. Empieza a usar Drive hoy con 10 GB gratis. Mejora tu almacenamiento cuando lo necesites, gratis por un mes, cancela cuando quieras.",
+        "security_title": "Internxt Security",
+        "security_subtitle": "Interxt usa tu contraseña para encriptar y decriptar tus archivos. Debido a la segura naturaleza de nuestro servicio, nosotros no sabemos tu contraseña. Esto significa que si la llegas a perder, tus archivos se volverán inaccesibles. Con nosotros, tu eres el único propietario de tus archivos. Te recomendamos encarecidamente que:",
+        "suggestion_1": "Guardes tu contraseña. Mantenla sana y salva.",
+        "suggestion_2": "Mantén una copia local de todos tus archivos.",
+        "create_account_title": "Crear cuenta"
+      },
+      "forgot_password": {
+        "title": "Internxt security",
+        "subtitle": "Como se especificó durante el proceso de registro, Internxt Drive encripta tus archivos, y solo tú toenees acceso a ellos. Nosotros nunca llegamos a conocer tu contrasñea, y debido a esto, solo tú puedes desencryptar tus archivos. Si te has olvidado de la contraseña, no podemos restablecerla. Lo que sí que podemos hacer, es borrar tu cuenta y todos tus archivos, por lo que podrás volver a registrarte con el mismo correo electrónico. Por favor, introduce tu correo electrónico para que podamos procesar el borrado de tu cuenta."
+      },
+      "deactivation_screen": {
+        "title": "Email de desactivación",
+        "subtitle_1": "Por favor, comprueba el email que te hemos enviado y sigue las intrucciones para desactivar tu cuenta para que puedas seguir usando Internxt Drive.",
+        "subtitle_2": "Una vez desactives tu cuenta, podrás volver a registrarte usando el mismo correo electrónico. Por favor guarda tu contraseña en un lugar seguro. Con Internxt Drive, solo tú eres el propietario de tus archivos. Un gran poder convella una gran responsabilidad."
+      },
+      "storage": {
+        "title": "Almacenamiento",
+        "space": {
+          "title": "Espacio de almacenamiento",
+          "used": {
+            "used": "Usado",
+            "of": "de"
+          },
+          "legend": {
+            "used": "Espacio usado",
+            "unused": "Espacio sin usar"
+          }
+        },
+        "plans": {
+          "title": "Planes de almacenamiento",
+          "title-2": "Duración del pago",
+          "current_plan": "Actualmente estás suscrito al plan de"
+        }
+      },
+      "photos": {
+        "screens": {
+          "photos": {
+            "albums": "Álbumes",
+            "all_photos": "Todas tus fotos"
+          },
+          "photo_gallery": {
+            "title": "Todas tus fotos",
+            "subtitle": "fotos"
+          }
+        }
+      }
+    },
+    "components": {
+      "buttons": {
+        "sign_in": "Iniciar sesión",
+        "create": "Crear cuenta",
+        "next": "Siguiente",
+        "get_started": "Empezar",
+        "continue": "Continuar",
+        "back": "Atrás",
+        "creating_button": "Creando...",
+        "deactivation": "Reenviar correo de desactivación",
+        "sing_up": "Registarse",
+        "descrypting": "Desencriptando...",
+        "cancel": "Cancelar",
+        "confirm": "Confirmar",
+        "move": "Mover",
+        "share": "Compartir"
+      },
+      "inputs": {
+        "email": "Correo electrónico",
+        "password": "Contraseña",
+        "confirm_password": "Confirmar contraseña",
+        "first_name": "Nombre",
+        "last_name": "Primer apellido"
+      },
+      "empty_folder": {
+        "title": "Esta carpeta está vacía",
+        "subtitle": "Aprieta en el botón de subida para empezar."
+      },
+      "app_menu": {
+        "search_box": "Buscar",
+        "filter": {
+          "date": "Fecha de subida",
+          "size": "Tamaño",
+          "name": "Nombre",
+          "type": "Tipo de archivo"
+        },
+        "upload": {
+          "title": "Selecciona el tipo de archivo",
+          "documen": "Sube un documento",
+          "media": "Sube multimedia",
+          "take_photo": "Haz una foto",
+          "create_folder": "Nombre de la carpeta"
+        },
+        "settings": {
+          "storage": "Almacenamiento",
+          "more": "Más información",
+          "photos": "Fotos",
+          "drive": "Drive",
+          "contact": "Contacto",
+          "sign": "Cerrar sesión"
+        }
+      },
+      "file_and_folder_options": {
+        "styling": "Color de la carpeta",
+        "icons": "Icono",
+        "type": "Tipo",
+        "size": "Tamaño",
+        "move": "Mover",
+        "share": "Compartir",
+        "delete": "Borrar"
+      }
+    },
+    "modals": {
+      "move_modal": {
+        "title": "Selecciona una carpeta a la que mover el archivo."
+      },
+      "share_modal": {
+        "title2": "Share your Drive file with this private link and enter the number of times you would like the link to be valid:",
+        "title": "Comparte este archivo con este link privado e introduce el limite de descargas posibles",
+        "loading": "Cargando link..."
+      },
+      "delete_modal": {
+        "title": "Borrar",
+        "subtitle": "Por favor, confirma que realmente quieres borrar este objeto. Esta acción no puede deshacerse."
+      }
+    }
+  }
+})
+
+export default strings
