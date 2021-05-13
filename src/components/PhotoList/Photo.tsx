e38f49284e512f54119c4d2da76f62ab9d2dbe4d
Aldimir <aldimirprincipal@gmail.com>
2021-05-13 16:49:14 +0200

    Main screen finished

--- src/components/PhotoList/Photo.tsx
+++ src/components/PhotoList/Photo.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { StyleSheet, Image, Dimensions, ActivityIndicator, View, Platform } from 'react-native';
 import FileViewer from 'react-native-file-viewer';
-import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import PhotoBadge from './PhotoBadge';
 import { cachePicture, downloadPhoto, IHashedPhoto } from '../../screens/Photos/init';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -34,7 +33,6 @@ export default function Photo(props: PhotoProps): JSX.Element {
 
   return (
     <TouchableOpacity
-      style={styles.imageView}
       onPress={() => {
         if (!item.localUri) {
           return;
@@ -62,34 +60,37 @@ export default function Photo(props: PhotoProps): JSX.Element {
       }}
       disabled={isDownloading}
     >
-      <Image
-        onLoadEnd={() => setIsLoaded(true)}
-        style={!isDownloading ? styles.image : [styles.image, styles.disabled]}
-        source={{ uri: item.localUri }}
-      />
+      <View style={{ width: (Dimensions.get('screen').width - 40) / 3, height: (Dimensions.get('screen').width - 80) / 3 }}>
+        <Image
+          onLoadEnd={() => setIsLoaded(true)}
+          style={{ width: '96%', height: '96%', alignSelf: 'center', borderRadius: 6 }}
+          resizeMode='cover'
+          source={{ uri: item.localUri }}
+        />
 
-      {!isLoaded || isDownloading
-        ? <ActivityIndicator color='gray' size='small' style={styles.badge} />
-        : <View style={styles.badge}>
-          {props.badge ||
-            <PhotoBadge
-              isUploaded={item.isUploaded}
-              isLocal={item.isLocal} />
+        {!isLoaded || isDownloading
+          ? <ActivityIndicator color='gray' size='small' style={styles.badge} />
+          : <View style={styles.badge}>
+            {props.badge ||
+              <PhotoBadge
+                isUploaded={item.isUploaded}
+                isLocal={item.isLocal} />
+            }
+          </View>
+        }
+
+        <View style={styles.progressIndicatorContainer}>
+          {
+            isDownloading ?
+              <LinearGradient
+                colors={['#47c7fd', '#096dff']}
+                start={[0, 0.7]}
+                end={[0.7, 1]}
+                style={[styles.progressIndicator, { width: (deviceWidth / 3.5) * progress }]} />
+              :
+              null
           }
         </View>
-      }
-
-      <View style={styles.progressIndicatorContainer}>
-        {
-          isDownloading ?
-            <LinearGradient
-              colors={['#47c7fd', '#096dff']}
-              start={[0, 0.7]}
-              end={[0.7, 1]}
-              style={[styles.progressIndicator, { width: (deviceWidth / 3.5) * progress }]} />
-            :
-            null
-        }
       </View>
     </TouchableOpacity>
   )
@@ -98,18 +99,6 @@ export default function Photo(props: PhotoProps): JSX.Element {
 const styles = StyleSheet.create({
   badge: {
     position: 'absolute'
-  },
-  disabled: {
-    opacity: 0.5
-  },
-  image: {
-    borderRadius: 10,
-    height: (deviceWidth - wp('30')) / 3,
-    width: (deviceWidth - wp('11')) / 3
-  },
-  imageView: {
-    backgroundColor: 'white',
-    borderRadius: 10
   },
   progressIndicator: {
     backgroundColor: '#87B7FF',
--- src/screens/PhotoGallery/index.tsx
+++ src/screens/PhotoGallery/index.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, Text, View, StyleSheet, Dimensions } from 'react-native';
 import { connect } from 'react-redux';
 import '../../../assets/icons/icon-back.png';
 import AlbumDetailsModal from '../../modals/AlbumDetailsModal';
 import AddItemToModal from '../../modals/AddItemToModal'
-import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import { Dispatch } from 'redux';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { WaveIndicator } from 'react-native-indicators';
 import { getPreviews, IHashedPhoto } from '../Photos/init';
 import strings from '../../../assets/lang/strings';
@@ -14,6 +14,15 @@ import { IPhotosToRender } from '../Photos';
 import { PhotoActions } from '../../redux/actions';
 import { tailwind } from '../../tailwind'
 import Syncing from '../../../assets/icons/photos/syncing.svg'
+import UploadBlue from '../../../assets/icons/photos/upload-file-blue.svg'
+import DownloadGray from '../../../assets/icons/photos/download-pending-gray.svg'
+import AlbumGray from '../../../assets/icons/photos/create-album-gray.svg'
+import Filter from '../../../assets/icons/photos/open-filters.svg'
+import Home from '../../../assets/icons/photos/home.svg'
+import FolderBlue from '../../../assets/icons/photos/folder-blue.svg'
+import SearchThinBlue from '../../../assets/icons/photos/search-thin-blue.svg'
+import AddMoreBlue from '../../../assets/icons/photos/add-more-blue.svg'
+import CrossWhite from '../../../assets/icons/photos/cross-white.svg'
 
 interface PhotoGalleryProps {
   navigation: any
@@ -21,6 +30,9 @@ interface PhotoGalleryProps {
   dispatch: Dispatch,
   isSyncing: boolean
 }
+
+const DEVICE_WIDTH = Dimensions.get('window').width
+const DEVICE_HEIGHT = Dimensions.get('window').height
 
 function PhotoGallery(props: PhotoGalleryProps): JSX.Element {
   const [photosToRender, setPhotosToRender] = useState<IHashedPhoto[]>(props.photosToRender.photos)
@@ -86,102 +98,84 @@ function PhotoGallery(props: PhotoGalleryProps): JSX.Element {
   }, [])
 
   return (
-    <View style={styles.container}>
+    <View style={tailwind('flex-1 px-5')}>
       <SafeAreaView>
         <AlbumDetailsModal />
         <AddItemToModal />
 
         <View style={tailwind('flex-col')}>
-          <View style={styles.titleContainer}>
-            <Text numberOfLines={1} style={styles.title}>
+          <View style={tailwind('flex-row')}>
+            <View style={tailwind('w-1/5')}></View>
+
+            <Text style={tailwind('w-3/5 text-center text-xl text-gray-80 font-averta-regular')}>
               {strings.screens.photos.screens.photo_gallery.title}
             </Text>
 
-            <View style={styles.headerButtonsContainer}>
-              <Syncing width={20} height={20} />
-              <Syncing width={20} height={20} />
-              <Syncing width={20} height={20} />
+            <View style={tailwind('flex-row w-1/5 justify-end')}>
+              <View style={tailwind('items-center justify-center mb-1')}>
+                <Syncing width={17} height={17} />
+              </View>
+
+              <View style={tailwind('bg-white h-6 w-6 rounded-sm items-center justify-center ml-2')}>
+                <UploadBlue width={20} height={15} />
+              </View>
+
+              <View style={tailwind('bg-white h-6 w-6 rounded-sm items-center justify-center ml-1')}>
+                <Filter width={20} height={13} />
+              </View>
             </View>
           </View>
 
-          <View style={styles.filterContainer}>
-            <View style={styles.filterButton}>
-              <Syncing width={20} height={20} />
+          <View style={tailwind('flex-row h-6 mt-1')}>
+            <View style={tailwind('flex-row w-1/3 bg-white rounded-l-md items-center justify-center')}>
+              <DownloadGray width={15} height={15} />
+              <Text style={tailwind('text-xs text-gray-80 font-averta-light ml-2')}>Download</Text>
             </View>
 
-            <View style={styles.filterButton}>
-              <Syncing width={20} height={20} />
+            <View style={tailwind('flex-row w-1/3 bg-white items-center justify-center ml-px mr-px')}>
+              <UploadBlue width={15} height={15} />
+              <Text style={tailwind('text-xs text-blue-60 font-averta-light ml-2')}>Upload pending</Text>
             </View>
 
-            <View style={styles.filterButton}>
-              <Syncing width={20} height={20} />
+            <View style={tailwind('flex-row w-1/3 bg-white rounded-r-md items-center justify-center')}>
+              <AlbumGray width={15} height={14} />
+              <Text style={tailwind('text-xs text-gray-80 font-averta-light ml-1')}>Album</Text>
             </View>
           </View>
         </View>
-        {/*
-        {
-          !props.isSyncing ?
-            null
-            :
-            <View style={styles.containerSync}>
-              <Text style={styles.syncText}>{strings.screens.photos.components.syncing}</Text>
 
-              <View>
-                <MaterialIndicator style={styles.spinner} color="#5291ff" size={15} />
-              </View>
-            </View>
-        } */}
         {
           photosToRender.length ?
             <FlatList
               data={photosToRender}
               numColumns={3}
               keyExtractor={item => item.hash}
-              contentContainerStyle={styles.flatList}
               renderItem={({ item }) => <Photo item={item} key={item.hash} pushDownloadedPhoto={pushDownloadedPhoto} />}
+              style={[tailwind('mt-2'), { height: DEVICE_HEIGHT * 0.78 }]}
             />
             :
             <WaveIndicator color="#5291ff" size={50} />
         }
+
+        <View style={tailwind('flex-row h-12 justify-between mt-6 pl-2')}>
+          <Home width={20} height={20} />
+          <FolderBlue width={20} height={20} />
+          <SearchThinBlue width={20} height={20} />
+          <AddMoreBlue width={20} height={20} />
+
+          <TouchableOpacity style={tailwind('flex-row h-6 w-20 px-2 bg-blue-60 rounded-xl items-center justify-around')}>
+            <CrossWhite width={12} height={12} />
+            <Text style={tailwind('text-white font-averta-regular text-sm')}>Album</Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: wp(4)
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    borderWidth: 1,
-    borderColor: 'red'
-  },
-  title: {
-    textAlign: 'center',
-    color: '#5E6C84',
-    fontFamily: 'Averta-Regular',
-    fontSize: 18,
-    borderWidth: 1
-  },
-  headerButtonsContainer: {
-    borderWidth: 1,
-    flexDirection: 'row'
-  },
-  filterContainer: {
-    flexDirection: 'row'
-  },
-  filterButton: {
-    flex: (1 / 3),
-    height: 20,
-    backgroundColor: 'white',
-    borderWidth: 1
-  },
-  flatList: {
-    paddingHorizontal: wp('0.5')
-  }
-});
+
+})
 
 const mapStateToProps = (state: any) => {
   return {
