596d1a0ad2557990dbe428840120ee7ce9f2bfa9
Aldimir <aldimirprincipal@gmail.com>
2021-06-16 12:22:30 +0200

    Changed version and build number to 1.3.5

--- src/components/PhotoList/Photo.tsx
+++ src/components/PhotoList/Photo.tsx
@@ -84,7 +84,7 @@ const Photo = ({ badge, item, dispatch, photoSelection, handleSelection }: Photo
     >
       <View style={{ width: (DEVICE_WIDTH - 40) / 3, height: (DEVICE_WIDTH - 80) / 3 }}>
         <View style={tailwind('m-0.5')}>
-          <View style={item.isUploading || item.isDownloading ? tailwind('absolute bg-gray-70 bg-opacity-40 w-full h-full rounded-md z-10') : ''} />
+          <View style={item.isUploading || item.isDownloading ? tailwind('absolute bg-gray-70 bg-opacity-50 w-full h-full rounded-md z-10') : ''} />
           <FastImage
             onLoadEnd={() => setIsLoaded(true)}
             style={tailwind('self-center rounded-md w-full h-full')}
