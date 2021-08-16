e9deb43b18e291e5774a25c46e6d96909d8abebe
zhihaoSAR <zuiaiduola1@gmail.com>
2021-08-16 11:18:08 +0200

    fix defailtModal style and refactor setting item

--- src/modals/FileDetailsModal/index.tsx
+++ src/modals/FileDetailsModal/index.tsx
@@ -1,13 +1,13 @@
 import prettysize from 'prettysize';
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import Modal from 'react-native-modalbox'
 import TimeAgo from 'react-native-timeago';
 import { connect } from 'react-redux';
 import Separator from '../../components/Separator';
 import { fileActions, layoutActions } from '../../redux/actions';
 import SettingsItem from '../SettingsModal/SettingsItem';
-import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import strings from '../../../assets/lang/strings';
 import { Reducers } from '../../redux/reducers/reducers';
 import * as Unicons from '@iconscout/react-native-unicons';
@@ -38,130 +38,17 @@ function FileDetailsModal(props: FileDetailsProps) {
 
   return <>
     {
-      isFolder ?
-        <Modal
-          position={'bottom'}
-          isOpen={showModal}
-          swipeArea={100}
-          swipeToClose={true}
-          coverScreen={true}
-          style={[styles.modal, styles.modalFolder]}
-          onClosed={async () => {
-            props.dispatch(fileActions.deselectAll())
-            props.dispatch(layoutActions.closeItemModal())
+      <Modal
+        position={'bottom'}
+        swipeArea={20}
+        style={[styles.modal, styles.modalSettingsFile]}
+        coverScreen={true}
+        isOpen={showModal}
+        onClosed={async () => {
+          props.dispatch(fileActions.deselectAll())
+          props.dispatch(layoutActions.closeItemModal())
 
-            // OLD MODAL was a form to edit on the fly the file metadata. Now is just a simple modal with links.
-            // The next code should be migrated to a new component.
-            /*
-            const metadata: IMetadata = {
-              itemName: ''
-            }
-
-            if (newfilename !== originalfilename) {
-              metadata.itemName = newfilename
-            }
-
-            if (Object.keys(metadata).length > 0) {
-              await updateFolderMetadata(metadata, folder.id)
-
-              props.dispatch(fileActions.getFolderContent(folder.parentId))
-              if (newfilename !== originalfilename) {
-                const userData = await getLyticsData()
-
-                analytics.track('folder-rename', {
-                  userId: userData.uuid,
-                  email: userData.email,
-                  platform: 'mobile',
-                  device: Platform.OS,
-                  // eslint-disable-next-line camelcase
-                  folder_id: folder.id
-                }).catch(() => { })
-              }
-            }
-            */
-          }}
-          backButtonClose={true}
-          animationDuration={200}
-        >
-          <View style={styles.drawerKnob}></View>
-
-          <Text
-            numberOfLines={1}
-            ellipsizeMode={'tail'}
-            style={styles.folderName}
-          >{file.name}</Text>
-
-          <Separator />
-
-          <View>
-            <TouchableHighlight
-              underlayColor={'#eee'}
-              onPress={() => {
-                // props.dispatch(layoutActions.closeItemModal())
-                props.dispatch(layoutActions.openRenameModal())
-              }}
-            >
-              <View style={{ flexDirection: 'row', alignItems: 'center', padding: 20, paddingLeft: 20 }}>
-                <View style={{ paddingRight: 10 }}>
-                  <Unicons.UilEdit color="#0F62FE" />
-                </View>
-                <View>
-                  <Text style={{ fontFamily: 'NeueEinstellung-Regular' }}>{strings.generic.rename}</Text>
-                </View>
-
-              </View>
-            </TouchableHighlight>
-          </View>
-
-          <View>
-            <TouchableHighlight
-              underlayColor={'#eee'}
-              onPress={() => {
-                // props.dispatch(layoutActions.closeItemModal())
-                props.dispatch(layoutActions.openDeleteModal())
-              }}>
-              <View
-                style={{ flexDirection: 'row', alignItems: 'center', padding: 20, paddingLeft: 20 }}>
-                <View style={{ paddingRight: 10 }}>
-                  <Unicons.UilTrashAlt color="#DA1E28"/>
-                </View>
-                <View>
-                  <Text style={{ fontFamily: 'NeueEinstellung-Regular', color: '#DA1E28' }}>{strings.generic.delete}</Text>
-                </View>
-              </View>
-            </TouchableHighlight>
-          </View>
-
-          <Separator />
-
-          <View>
-            <TouchableHighlight
-              underlayColor={'#eee'}
-              style={{
-                alignItems: 'center',
-                padding: 20
-              }}
-              onPress={() => {
-                props.dispatch(fileActions.deselectAll())
-                props.dispatch(layoutActions.closeItemModal())
-              }}>
-              <Text style={{ color: '#DA1E28' }}>{strings.generic.cancel}</Text>
-            </TouchableHighlight>
-          </View>
-
-        </Modal>
-        :
-        <Modal
-          position={'bottom'}
-          swipeArea={20}
-          style={[styles.modal, styles.modalSettingsFile]}
-          coverScreen={true}
-          isOpen={showModal}
-          onClosed={async () => {
-            props.dispatch(fileActions.deselectAll())
-            props.dispatch(layoutActions.closeItemModal())
-
-            /*
+          /*
             const metadata: IMetadata = {
               itemName: ''
             }
@@ -182,19 +69,20 @@ function FileDetailsModal(props: FileDetailsProps) {
               }).catch(() => { })
             }
             */
-          }}
-          backButtonClose={true}
-          backdropPressToClose={true}
-          animationDuration={200}
+        }}
+        backButtonClose={true}
+        backdropPressToClose={true}
+        animationDuration={200}
+      >
+        <View style={styles.drawerKnob}></View>
+
+        <View
+          style={styles.fileName}
         >
-          <View style={styles.drawerKnob}></View>
+          <Text style={styles.fileName}>{newfilename}{file && file.type ? '.' + file.type : ''}</Text>
+        </View>
 
-          <View
-            style={styles.fileName}
-          >
-            <Text style={{ fontSize: 15, textAlign: 'center', margin: 10 }}>{newfilename}{file && file.type ? '.' + file.type : ''}</Text>
-          </View>
-
+        {isFolder ? <></> : <View>
           <Separator />
 
           <View style={styles.infoContainer}>
@@ -219,11 +107,12 @@ function FileDetailsModal(props: FileDetailsProps) {
               </Text>
             </Text>
           </View>
+        </View>
+        }
+        <Separator />
 
-          <Separator />
-
-          <View style={styles.optionsContainer}>
-            {/*
+        <View style={styles.optionsContainer}>
+          {/*
             <SettingsItem
               text={
                 <Text>
@@ -237,46 +126,43 @@ function FileDetailsModal(props: FileDetailsProps) {
               }}
             />
             */}
-
+          {isFolder? <></> :
             <SettingsItem
-              text={<View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <View style={{ margin: 10 }}>
-                  <Unicons.UilShare color="#0F62FE" />
-                </View>
-                <Text style={{}}>{strings.components.file_and_folder_options.share}</Text>
-              </View>}
+              text={strings.components.file_and_folder_options.share}
+              icon={Unicons.UilShare}
               onPress={() => {
                 props.dispatch(layoutActions.closeItemModal())
                 props.dispatch(layoutActions.openShareModal())
               }}
             />
+          }
+          <SettingsItem
+            text={strings.generic.rename}
+            icon={Unicons.UilEdit}
+            onPress={() => {
+              props.dispatch(layoutActions.openRenameModal())
+            }}
+          />
 
-            <SettingsItem
-              text={<View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <View style={{ margin: 10 }}>
-                  <Unicons.UilShare color="#0F62FE" />
-                </View>
-                <Text style={{}}>{strings.generic.rename}</Text>
-              </View>}
-              onPress={() => {
-                props.dispatch(layoutActions.openRenameModal())
-              }}
-            />
-
-            <SettingsItem
-              text={<View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <View style={{ margin: 10 }}>
-                  <Unicons.UilTrashAlt color="#DA1E28" />
-                </View>
-
-                <Text style={styles.cerebriSansBold}>{strings.components.file_and_folder_options.delete}</Text>
-              </View>}
-              onPress={() => {
-                props.dispatch(layoutActions.openDeleteModal())
-              }}
-            />
-          </View>
-        </Modal>}
+          <SettingsItem
+            text={strings.components.file_and_folder_options.delete}
+            icon={Unicons.UilTrashAlt}
+            color={'#DA1E28'}
+            onPress={() => {
+              props.dispatch(layoutActions.openDeleteModal())
+            }}
+          />
+        </View>
+        <Separator />
+        <View style={styles.cancelContainer}>
+          <SettingsItem
+            text={<Text style={styles.cancelText}>{strings.generic.cancel}</Text>}
+            onPress={() => {
+              props.dispatch(layoutActions.closeItemModal());
+            }}
+          />
+        </View>
+      </Modal>}
   </>;
 }
 
@@ -305,17 +191,12 @@ const styles = StyleSheet.create({
   fileName: {
     width: wp(92),
     alignSelf: 'center',
-    fontFamily: 'NeueEinstellung-Bold',
-    fontSize: 20,
-    padding: 0 // remove default padding on Android
-  },
-  folderName: {
-    fontFamily: 'NeueEinstellung-Bold',
     textAlign: 'center',
-    fontSize: 20,
-    marginLeft: 20,
-    marginRight: 20,
-    padding: 0 // Remove default padding Android
+    fontFamily: 'NeueEinstellung-Bold',
+    fontSize: 16,
+    padding: 0, // remove default padding on Android
+    fontWeight: 'bold',
+    color: '#42526E'
   },
   infoContainer: {
     height: 'auto'
@@ -323,10 +204,6 @@ const styles = StyleSheet.create({
   modal: {
     borderTopLeftRadius: 32,
     borderTopRightRadius: 32
-  },
-  modalFolder: {
-    height: hp('90%') < 550 ? 550 : Math.min(380, hp('90%')),
-    marginTop: wp('12')
   },
   modalSettingsFile: {
     height: 'auto'
@@ -340,5 +217,19 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     paddingBottom: 6,
     paddingLeft: 24
+  },
+  cancelText: {
+    color: '#f00',
+    textAlign: 'center',
+    flexGrow: 1,
+    fontFamily: 'NeueEinstellung-Regular',
+    fontSize: 19,
+    fontWeight: '500'
+  },
+  cancelContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    flexGrow: 1,
+    marginBottom: 16
   }
 })
