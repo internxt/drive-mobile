671497de167873ca50a16538b7fb72f664a2e1ae
zhihaoSAR <zuiaiduola1@gmail.com>
2021-08-24 12:15:46 +0200

    add keyboardAvoiding to change password screen

--- src/screens/ChangePassword/index.tsx
+++ src/screens/ChangePassword/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, TextInput, TouchableHighlight } from 'react-native';
+import { View, StyleSheet, Text, TextInput, TouchableHighlight, KeyboardAvoidingView } from 'react-native';
 import { isNullOrEmpty, isStrongPassword } from '../Register/registerUtils';
 import { connect } from 'react-redux';
 import AppMenu from '../../components/AppMenu';
@@ -44,77 +44,79 @@ function ChangePassword(props: any) {
     backgroundColor: 'white',
     flex: 1
   }}>
-    <AppMenu
-      title={strings.components.inputs.password}
-      onBackPress={() => {
-        props.navigation.goBack()
-      }}
-      hideSearch={true} hideOptions={true} />
-    <View style={styles.mainContainer}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.titleText}>{strings.screens.change_password.title}</Text>
-      </View>
-      <View style={styles.titleContainer}>
-        <Text style={styles.subtitleText}>{strings.screens.change_password.warning}</Text>
-      </View>
-      <View style={styles.container}>
-        <View style={[tailwind('input-wrapper my-2'), tailwind(password === '' ? '' : (isValidPassword ? 'input-valid' : 'input-error'))]}>
-          <TextInput
-            style={tailwind('input')}
-            value={password}
-            onChangeText={value => setPassword(value)}
-            placeholder={strings.components.inputs.password}
-            placeholderTextColor="#666"
-            secureTextEntry={true}
-            textContentType="password"
-            onFocus={() => setPasswordFocus(true)}
-            onBlur={() => setPasswordFocus(false)}
-          />
-          <Unicons.UilEye
-            style={[tailwind('input-icon'), { display: 'none' }]}
-            color={passwordFocus && isValidPassword ? '#42BE65' : '#7A869A'} />
+    <KeyboardAvoidingView behavior='position'>
+      <AppMenu
+        title={strings.components.inputs.password}
+        onBackPress={() => {
+          props.navigation.goBack()
+        }}
+        hideSearch={true} hideOptions={true} />
+      <View style={styles.mainContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.titleText}>{strings.screens.change_password.title}</Text>
         </View>
-        <View style={[tailwind('input-wrapper my-2'), tailwind(newPassword === '' ? '' : (isValidNewPassword ? 'input-valid' : 'input-error'))]}>
-          <TextInput
-            style={tailwind('input')}
-            value={newPassword}
-            onChangeText={value => setNewPassword(value)}
-            placeholder='New password'
-            placeholderTextColor="#666"
-            secureTextEntry={true}
-            textContentType="password"
-            onFocus={() => setNewPasswordFocus(true)}
-            onBlur={() => setNewPasswordFocus(false)}
-          />
-          <Unicons.UilEye
-            style={[tailwind('input-icon'), { display: 'none' }]}
-            color={newPasswordFocus && isValidNewPassword ? '#42BE65' : '#7A869A'} />
+        <View style={styles.titleContainer}>
+          <Text style={styles.subtitleText}>{strings.screens.change_password.warning}</Text>
         </View>
-        <View style={[tailwind('input-wrapper my-2'), tailwind(confirmPassword === '' ? '' : (passwordConfirmed ? 'input-valid' : 'input-error'))]}>
-          <TextInput
-            style={tailwind('input')}
-            value={confirmPassword}
-            onChangeText={value => setConfirmPassword(value)}
-            placeholder={strings.components.inputs.confirm_password}
-            placeholderTextColor="#666"
-            secureTextEntry={true}
-            textContentType="password"
-            onFocus={() => setConfirmPasswordFocus(true)}
-            onBlur={() => setConfirmPasswordFocus(false)}
-          />
-          <Unicons.UilEye
-            style={[tailwind('input-icon'), { display: 'none' }]}
-            color={confirmPasswordFocus && passwordConfirmed ? '#42BE65' : '#7A869A'} />
+        <View style={styles.container}>
+          <View style={[tailwind('input-wrapper my-2'), tailwind(password === '' ? '' : (isValidPassword ? 'input-valid' : 'input-error'))]}>
+            <TextInput
+              style={tailwind('input')}
+              value={password}
+              onChangeText={value => setPassword(value)}
+              placeholder={strings.components.inputs.password}
+              placeholderTextColor="#666"
+              secureTextEntry={true}
+              textContentType="password"
+              onFocus={() => setPasswordFocus(true)}
+              onBlur={() => setPasswordFocus(false)}
+            />
+            <Unicons.UilEye
+              style={[tailwind('input-icon'), { display: 'none' }]}
+              color={passwordFocus && isValidPassword ? '#42BE65' : '#7A869A'} />
+          </View>
+          <View style={[tailwind('input-wrapper my-2'), tailwind(newPassword === '' ? '' : (isValidNewPassword ? 'input-valid' : 'input-error'))]}>
+            <TextInput
+              style={tailwind('input')}
+              value={newPassword}
+              onChangeText={value => setNewPassword(value)}
+              placeholder='New password'
+              placeholderTextColor="#666"
+              secureTextEntry={true}
+              textContentType="password"
+              onFocus={() => setNewPasswordFocus(true)}
+              onBlur={() => setNewPasswordFocus(false)}
+            />
+            <Unicons.UilEye
+              style={[tailwind('input-icon'), { display: 'none' }]}
+              color={newPasswordFocus && isValidNewPassword ? '#42BE65' : '#7A869A'} />
+          </View>
+          <View style={[tailwind('input-wrapper my-2'), tailwind(confirmPassword === '' ? '' : (passwordConfirmed ? 'input-valid' : 'input-error'))]}>
+            <TextInput
+              style={tailwind('input')}
+              value={confirmPassword}
+              onChangeText={value => setConfirmPassword(value)}
+              placeholder={strings.components.inputs.confirm_password}
+              placeholderTextColor="#666"
+              secureTextEntry={true}
+              textContentType="password"
+              onFocus={() => setConfirmPasswordFocus(true)}
+              onBlur={() => setConfirmPasswordFocus(false)}
+            />
+            <Unicons.UilEye
+              style={[tailwind('input-icon'), { display: 'none' }]}
+              color={confirmPasswordFocus && passwordConfirmed ? '#42BE65' : '#7A869A'} />
+          </View>
+          <TouchableHighlight
+            style={[tailwind('btn btn-primary my-5'), (activeButton && !isLoading) ? null : { backgroundColor: '#A6C8FF' }]}
+            underlayColor="#4585f5"
+            onPress={handleOnPress}
+            disabled={!activeButton || isLoading}>
+            <Text style={tailwind('text-base btn-label')}>{strings.screens.change_password.title}</Text>
+          </TouchableHighlight>
         </View>
-        <TouchableHighlight
-          style={[tailwind('btn btn-primary my-5'), (activeButton && !isLoading) ? null : { backgroundColor: '#A6C8FF' }]}
-          underlayColor="#4585f5"
-          onPress={handleOnPress}
-          disabled={!activeButton || isLoading}>
-          <Text style={tailwind('text-base btn-label')}>{strings.screens.change_password.title}</Text>
-        </TouchableHighlight>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   </View>
 
 }
