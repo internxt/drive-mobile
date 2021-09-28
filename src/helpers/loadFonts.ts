56e0d387d8368ca54a19fc8b556db0fe498b7e04
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-09-28 10:06:15 +0200

    global font

--- src/helpers/loadFonts.ts
+++ src/helpers/loadFonts.ts
@@ -1,10 +1,12 @@
 import * as Font from 'expo-font';
+import React from 'react'
+import { Text } from 'react-native'
 
 /**
  * Load fonts from assets
  */
 async function loadFontsAsync(): Promise<void> {
-  return Font.loadAsync({
+  await Font.loadAsync({
     'NeueEinstellung-Black': require('../../assets/fonts/NeueEinstellung-Black.otf'),
     'NeueEinstellung-Bold': require('../../assets/fonts/NeueEinstellung-Black.otf'),
     'NeueEinstellung-ExtraBold': require('../../assets/fonts/NeueEinstellung-ExtraBold.otf'),
@@ -15,6 +17,16 @@ async function loadFontsAsync(): Promise<void> {
     'NeueEinstellung-SemiBold': require('../../assets/fonts/NeueEinstellung-SemiBold.otf'),
     'NeueEinstellung-Thin': require('../../assets/fonts/NeueEinstellung-Thin.otf')
   });
+
+  const oldRender = Text.render;
+
+  Text.render = function (...args) {
+    const origin = oldRender.call(this, ...args);
+
+    return React.cloneElement(origin, {
+      style: [{ fontFamily: 'NeueEinstellung-Regular' }, origin.props.style]
+    });
+  };
 }
 
 export const loadFonts = loadFontsAsync
