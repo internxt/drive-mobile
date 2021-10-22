29792ee6650a004e1fb053b41d135f186b6ec041
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-10-22 08:48:40 +0200

    photoelement removed

--- src/screens/Photos/Photos.tsx
+++ src/screens/Photos/Photos.tsx
@@ -1,20 +1,21 @@
 import CameraRoll from '@react-native-community/cameraroll';
 import React, { useEffect, useState } from 'react';
-import { View, Text, ListRenderItemInfo, SafeAreaView, TouchableOpacity, RefreshControl } from 'react-native';
+import { View, Text, ListRenderItemInfo, SafeAreaView, TouchableOpacity, RefreshControl, Dimensions, Image } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import { connect } from 'react-redux';
 import { tailwind } from '../../helpers/designSystem';
 import { Reducers } from '../../redux/reducers/reducers';
 import { loadLocalPhotos } from '../../services/photos';
 import globalStyle from '../../styles/global.style';
-import PhotoElement from './PhotoElement';
 
 function Photos(props: Reducers): JSX.Element {
-
   const [refreshing, setRefreshing] = useState(false)
   const [selectionMode, setSelectionMode] = useState(false);
   const [photos, setPhotos] = useState<CameraRoll.PhotoIdentifier[]>([]);
   const [photoCursor, setPhotoCursor] = useState<string>();
+
+  const screenWidth = Dimensions.get('window').width;
+  const thirdWidth = screenWidth / 3;
 
   const loadMorePhotos = async (cursor?: any) => {
     const [loadedPhotos, nextCursor] = await loadLocalPhotos(cursor);
@@ -27,19 +28,19 @@ function Photos(props: Reducers): JSX.Element {
 
   useEffect(() => {
     if (process.env.NODE_ENV === 'development') {
-      setPhotos([]);
+      // setPhotos([]);
     }
     loadMorePhotos();
   }, []);
 
-  return <SafeAreaView>
+  return <SafeAreaView style={tailwind('bg-white')}>
     <View style={tailwind('flex-row items-center justify-between absolute z-10 w-full p-3')}>
       <View>
-        <Text style={tailwind('text-3xl')}>Gallery</Text>
+        <Text style={[tailwind('text-3xl'), globalStyle.fontWeight.medium]}>Gallery</Text>
       </View>
-      <View style={tailwind('')}>
-        <Text>Select</Text>
-      </View>
+      <TouchableOpacity style={tailwind('bg-blue-10 px-3 py-2 rounded-3xl')}>
+        <Text style={[tailwind('text-blue-60'), globalStyle.fontWeight.medium]}>Select</Text>
+      </TouchableOpacity>
     </View>
     <FlatList
       showsVerticalScrollIndicator={true}
@@ -53,7 +54,6 @@ function Photos(props: Reducers): JSX.Element {
           }}
         />
       }
-
       decelerationRate={0.5}
       contentContainerStyle={tailwind('mt-16')}
       data={photos}
@@ -61,7 +61,18 @@ function Photos(props: Reducers): JSX.Element {
       onEndReached={() => loadMorePhotos(photoCursor)}
       onEndReachedThreshold={3}
       renderItem={(item: ListRenderItemInfo<CameraRoll.PhotoIdentifier>) => {
-        return <PhotoElement item={item.item} />
+        const uri = item.item.node.image.uri;
+
+        return <TouchableOpacity
+          onPress={() => {
+            props.navigation.push('Preview', { uri })
+          }}
+          key={item.item.node.image.uri}
+          style={[tailwind('p-0.5'), { width: thirdWidth, height: thirdWidth }]}>
+          <Image
+            style={tailwind('w-full h-full')}
+            source={{ uri }} />
+        </TouchableOpacity>
       }}
     />
   </SafeAreaView>;
