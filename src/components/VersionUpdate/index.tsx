5fd93e41246eca10f03aed9d03fb420c3a9f5c8d
Carlos Sala Samper <carlos15695@hotmail.com>
2021-12-15 12:22:57 +0100

    versionUpdate version 2

--- src/components/VersionUpdate/index.tsx
+++ src/components/VersionUpdate/index.tsx
@@ -76,7 +76,7 @@ function VersionUpdate(props: Reducers): JSX.Element {
     }
     <View>
       <Text style={tailwind('text-center text-base text-sm text-gray-50')}>
-        Internxt Drive v{PackageJson.version} (13)
+        Internxt Drive v{PackageJson.version} (2)
       </Text>
     </View>
   </>
