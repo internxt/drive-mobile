d9f23734c0af513ffbe1be590681803c2a15207e
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-12-21 22:05:18 +0100

    upload steps refactored

---
+++ src/services/network/index.ts
@@ -0,0 +1,236 @@
+import { randomBytes, createCipheriv, Cipher } from 'react-native-crypto';
+import RFNS, { UploadFileItem } from 'react-native-fs';
+import { eachLimit } from 'async';
+import uuid from 'react-native-uuid';
+import axios, { AxiosRequestConfig } from 'axios';
+
+import { GenerateFileKey, ripemd160, sha256, sha512HmacBuffer } from '../../@inxt-js/lib/crypto';
+import { getDocumentsDir } from '../../lib/fs';
+import { ShardMeta } from '../../@inxt-js/lib/shardMeta';
+
+const networkApiUrl = process.env.NETWORK_PHOTOS_API_URL as string;
+
+type FrameId = string;
+type FileId = string;
+type BucketId = string;
+type Mnemonic = string;
+type NetworkUser = string;
+type NetworkPass = string;
+type BucketEntryId = string;
+type FileEncryptedURI = string;
+
+interface NetworkCredentials {
+  encryptionKey: Mnemonic
+  user: NetworkUser
+  pass: NetworkPass
+}
+
+interface Contract {
+  hash: string;
+  token: string;
+  operation: 'PUSH';
+  url: string;
+  farmer: {
+    userAgent: string;
+    protocol: string;
+    address: string;
+    port: number;
+    nodeID: string;
+    lastSeen: number;
+  };
+}
+
+interface Frame {
+  id: FrameId;
+  user: string;
+  shards: [];
+  storageSize: number;
+  size: number;
+  locked: boolean;
+  created: string;
+}
+
+interface CreateEntryFromFrameBody {
+  frame: string;
+  filename: string;
+  index: string;
+  hmac: {
+    type: string;
+    value: string;
+  }
+  erasure?: {
+    type: string;
+  }
+}
+
+function bucketExists(bucketId: BucketId, options?: AxiosRequestConfig): Promise<boolean> {
+  return axios.get(`${networkApiUrl}/buckets/${bucketId}`, options)
+    .then(() => true);
+}
+
+function stageFile(options?: AxiosRequestConfig): Promise<FrameId> {
+  return axios.post<Frame>(`${networkApiUrl}/frames`, options)
+    .then((res) => res.data.id);
+}
+
+function negotiateContract(frameId: FrameId, shardMeta: ShardMeta): Promise<Contract> {
+  return axios.put<Contract>(`${networkApiUrl}/frames/${frameId}`, {}, {
+    data: { ...shardMeta, challenges: shardMeta.challenges_as_str }
+  }).then((res) => {
+    return res.data;
+  });
+}
+
+function generateHmac(encryptionKey: Buffer, shardMetas: ShardMeta[]): string {
+  const shardMetasCopy = [...shardMetas].sort((sA, sB) => sA.index - sB.index);
+  const hmac = sha512HmacBuffer(encryptionKey);
+
+  for (const shardMeta of shardMetasCopy) {
+    hmac.update(Buffer.from(shardMeta.hash, 'hex'));
+  }
+
+  return hmac.digest().toString('hex');
+}
+
+async function createBucketEntry(frameId: FrameId, encryptionKey: Buffer, index: Buffer, shardMetas: ShardMeta[]): Promise<BucketEntryId> {
+  const hmac = generateHmac(encryptionKey, shardMetas);
+  // TODO: Encrypt this
+  const filename = uuid.v4().toString();
+
+  const newBucketEntry = generateBucketEntry(frameId, filename, index, hmac, false);
+  // TODO: Make bridge request
+}
+
+function generateBucketEntry(
+  frameId: FrameId,
+  filename: string,
+  index: Buffer,
+  hmac: string,
+  rs: boolean,
+): CreateEntryFromFrameBody {
+  const bucketEntry: CreateEntryFromFrameBody = {
+    frame: frameId,
+    filename,
+    index: index.toString('hex'),
+    hmac: { type: 'sha512', value: hmac },
+  };
+
+  if (rs) {
+    bucketEntry.erasure = { type: 'reedsolomon' };
+  }
+
+  return bucketEntry;
+}
+
+function generateCipher(key: Buffer, iv: Buffer): Cipher {
+  return createCipheriv('aes-256-ctr', key, iv);
+}
+
+function encryptFile(fileUri: string, fileSize: number, cipher: Cipher): Promise<FileEncryptedURI> {
+  const twoMb = 2 * 1024 * 1024;
+  const chunksOf = twoMb;
+  const chunks = Math.ceil(fileSize / twoMb);
+
+  let start = 0;
+
+  const fileEncryptedURI: FileEncryptedURI = getDocumentsDir() + 'hola.enc';
+
+  return eachLimit(new Array(chunks), 1, (_, cb) => {
+    RFNS.read(fileUri, chunksOf, start, 'base64')
+      .then((res) => {
+        cipher.write(Buffer.from(res, 'base64'));
+        start += twoMb;
+        return RFNS.appendFile(fileEncryptedURI, cipher.read().toString('base64'), 'base64');
+      })
+      .then(() => cb(null))
+      .catch(cb);
+  }).then(() => {
+    return fileEncryptedURI;
+  });
+}
+
+export async function uploadFile(fileURI: string, bucketId: BucketId, credentials: NetworkCredentials): Promise<FileId> {
+  if (!bucketId) {
+    throw new Error('Upload error code 1');
+  }
+
+  if (!credentials.encryptionKey) {
+    throw new Error('Upload error code 2');
+  }
+
+  if (!credentials.user) {
+    throw new Error('Upload error code 3');
+  }
+
+  if (!credentials.pass) {
+    throw new Error('Upload error code 4');
+  }
+
+  const defaultRequestOptions: AxiosRequestConfig = {
+    auth: {
+      username: credentials.user,
+      password: sha256(Buffer.from(credentials.pass)).toString('hex')
+    }
+  };
+
+  // 1. Check bucket existence
+  const exists = await bucketExists(bucketId, defaultRequestOptions);
+
+  if (!exists) {
+    throw new Error('Bucket not exists');
+  }
+
+  // 2. Stage frame
+  const frameId = await stageFile(defaultRequestOptions);
+
+  // 3. Generate cipher
+  const index = randomBytes(32);
+  const fileEncryptionKey = await GenerateFileKey(credentials.encryptionKey, bucketId, index);
+  const cipher = generateCipher(fileEncryptionKey, index.slice(0, 16));
+
+  // 4. Encrypt file
+  const fileStats = await RFNS.stat(fileURI);
+  const fileSize = parseInt(fileStats.size);
+  const fileEncryptedURI = await encryptFile(fileURI, fileSize, cipher);
+
+  // TODO: Buffer from what? Hex?
+  const fileHash = ripemd160(
+    Buffer.from(await RFNS.hash(fileEncryptedURI, 'sha256'), 'hex')
+  );
+
+  const shardMetas: ShardMeta[] = [{
+    index: 0,
+    hash: fileHash.toString('hex'),
+    parity: false,
+    size: fileSize,
+    tree: [],
+    challenges_as_str: [],
+    challenges: [],
+  }];
+
+  // 5. Negotiate contract
+  const contract = await negotiateContract(frameId, shardMetas[0]);
+
+  // 6. Upload file
+  const files: UploadFileItem[] = [{
+    filename: '',
+    filepath: fileEncryptedURI,
+    filetype: '',
+    name: ''
+  }];
+
+  const uploadResult = RFNS.uploadFiles({
+    toUrl: contract.url,
+    // binaryStreamOnly
+    files,
+    method: 'POST',
+    progress: (res) => {
+      console.log(((res.totalBytesSent / res.totalBytesExpectedToSend) * 100).toFixed(2));
+    }
+  });
+
+  await uploadResult.promise;
+
+  // 7. Create file entry
+  return createBucketEntry(frameId, fileEncryptionKey, index, shardMetas);
+}
