7e5391fe51392fb2e9fbe013cf7ca20df6596c11
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-12-29 13:24:30 +0100

    load image on preview

--- src/screens/PhotosNavigator/PhotosPreviewScreen/index.tsx
+++ src/screens/PhotosNavigator/PhotosPreviewScreen/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import RNFS from 'react-native-fs';
 import { View, Text, Image, SafeAreaView, TouchableOpacity } from 'react-native';
 import { TapGestureHandler } from 'react-native-gesture-handler';
 import tailwind, { getColor } from 'tailwind-rn';
@@ -6,7 +7,6 @@ import * as Unicons from '@iconscout/react-native-unicons';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useNavigation } from '@react-navigation/native';
 import { NavigationStackProp } from 'react-navigation-stack';
-import { Photo } from '@internxt/sdk';
 
 import PhotosPreviewOptionsModal from '../../../components/modals/PhotosPreviewOptionsModal';
 import DeletePhotosModal from '../../../components/modals/DeletePhotosModal';
@@ -14,6 +14,9 @@ import { useAppDispatch, useAppSelector } from '../../../store/hooks';
 import { layoutActions } from '../../../store/slices/layout';
 import strings from '../../../../assets/lang/strings';
 import SharePhotoModal from '../../../components/modals/SharePhotoModal';
+import { downloadFile } from '../../../services/network';
+import { getDocumentsDir } from '../../../lib/fs';
+import { Photo } from '../../../services/sync/types';
 
 interface PreviewProps {
   route: {
@@ -24,12 +27,20 @@ interface PreviewProps {
 }
 
 function PhotosPreviewScreen(props: PreviewProps): JSX.Element {
+  const photo = props.route.params.data;
+  /*
+    TODO: Transform from snake case to camel case when fields are queried on SQLite.
+    https://stackoverflow.com/a/38757038/9090874
+  */
+  const photoPath = getDocumentsDir() + '/' + photo.file_id;
+
   const dispatch = useAppDispatch();
   const [isOptionsModalOpen, setIsOptionsModalOpen] = useState(false);
+  const [uri, setUri] = useState('data:image/png;base64,' + photo.preview);
+  const [downloadFinished, setDownloadFinished] = useState(false);
+  const [progress, setProgress] = useState(0);
   const { isDeletePhotosModalOpen, isSharePhotoModalOpen } = useAppSelector((state) => state.layout);
   const navigation = useNavigation<NavigationStackProp>();
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const examplePhoto = require('../../../../assets/images/photos/example.png');
   const onBackButtonPressed = () => navigation.goBack();
   const onShareButtonPressed = () => dispatch(layoutActions.setIsSharePhotoModalOpen(true));
   const onDownloadButtonPressed = () => {
@@ -39,6 +50,40 @@ function PhotosPreviewScreen(props: PreviewProps): JSX.Element {
     dispatch(layoutActions.setIsDeletePhotosModalOpen(true));
   };
   const onSharePhotoModalClosed = () => dispatch(layoutActions.setIsSharePhotoModalOpen(false));
+
+  const isPhotoAlreadyDownloaded = () => {
+    return RNFS.exists(photoPath);
+  };
+
+  const loadImage = () => {
+    // TODO: Get creds from store
+    downloadFile('', props.route.params.data.file_id, {
+      encryptionKey: '',
+      pass: '',
+      user: ''
+    }, {
+      toPath: photoPath,
+      progressCallback: (progress) => {
+        setProgress(progress);
+      }
+    }).then((fileUri) => {
+      setUri(fileUri);
+      setDownloadFinished(true);
+    }).catch((err) => {
+      console.log(err.message);
+    });
+  };
+
+  useEffect(() => {
+    isPhotoAlreadyDownloaded().then((isDownloaded) => {
+      if (isDownloaded) {
+        setUri(photoPath);
+        setDownloadFinished(true);
+      } else {
+        loadImage();
+      }
+    });
+  }, []);
 
   return (
     <>
@@ -58,11 +103,10 @@ function PhotosPreviewScreen(props: PreviewProps): JSX.Element {
         data={props.route.params.data}
         onClosed={onSharePhotoModalClosed}
       />
-
       <View style={tailwind('h-full')}>
         {/* PHOTO */}
         <TapGestureHandler numberOfTaps={1} enabled={true}>
-          <Image resizeMode={'contain'} style={tailwind('bg-black w-full h-full absolute')} source={examplePhoto} />
+          <Image resizeMode={'contain'} style={tailwind('bg-black w-full h-full absolute')} source={{ uri }} />
         </TapGestureHandler>
 
         <SafeAreaView style={tailwind('flex-col justify-between h-full')}>
@@ -72,6 +116,7 @@ function PhotosPreviewScreen(props: PreviewProps): JSX.Element {
           >
             <View style={tailwind('flex-row justify-between p-5')}>
               {/* BACK BUTTON */}
+              <Text style={tailwind('text-white text-xs')}>{(progress * 100).toFixed(2) + '%'}</Text>
               <TouchableOpacity style={tailwind('z-10')} onPress={onBackButtonPressed}>
                 <Unicons.UilAngleLeft color={getColor('white')} size={32} />
               </TouchableOpacity>
@@ -91,8 +136,8 @@ function PhotosPreviewScreen(props: PreviewProps): JSX.Element {
               <Unicons.UilLink color="white" size={26} />
               <Text style={tailwind('text-white text-xs')}>{strings.components.buttons.shareWithLink}</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={tailwind('items-center')} onPress={onDownloadButtonPressed}>
-              <Unicons.UilImport color="white" size={26} />
+            <TouchableOpacity disabled={!downloadFinished} style={tailwind('items-center')} onPress={onDownloadButtonPressed}>
+              <Unicons.UilImport color='white' size={26} />
               <Text style={tailwind('text-white text-xs')}>{strings.components.buttons.download}</Text>
             </TouchableOpacity>
             <TouchableOpacity style={tailwind('items-center')} onPress={onMoveToTrashButtonPressed}>
