2c612ddbe83800898e2796c74fa05d9286ea70dd
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-12-29 13:22:41 +0100

    detect scroll direction on gallery

--- src/components/gallery-views/GalleryAllView.tsx
+++ src/components/gallery-views/GalleryAllView.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Photo } from '@internxt/sdk';
 import { Dimensions, FlatList, ListRenderItemInfo, RefreshControl, View } from 'react-native';
 import { NavigationStackProp } from 'react-navigation-stack';
 import { useNavigation } from '@react-navigation/native';
@@ -8,6 +7,7 @@ import { tailwind } from '../../helpers/designSystem';
 import GalleryItem from '../GalleryItem';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { PhotosScreen } from '../../types';
+import { Photo } from '../../services/sync/types';
 import { photosActions, photosSelectors, photosThunks } from '../../store/slices/photos';
 
 const GalleryAllView = (): JSX.Element => {
@@ -33,6 +33,9 @@ const GalleryAllView = (): JSX.Element => {
     isSelectionModeActivated ? onItemLongPressed(item) : navigation.push(PhotosScreen.Preview, { data: item });
   };
 
+  const limit = 50;
+  const offset = 0;
+
   return (
     <FlatList
       style={tailwind('bg-white')}
@@ -43,7 +46,7 @@ const GalleryAllView = (): JSX.Element => {
           refreshing={refreshing}
           onRefresh={async () => {
             setRefreshing(true);
-            await dispatch(photosThunks.loadLocalPhotosThunk({}));
+            await dispatch(photosThunks.loadLocalPhotosThunk({ limit: 10, offset: 0 }));
             setRefreshing(false);
           }}
         />
@@ -51,6 +54,16 @@ const GalleryAllView = (): JSX.Element => {
       decelerationRate={0.5}
       ItemSeparatorComponent={() => <View style={{ height: gutter }} />}
       data={photos}
+      onScrollEndDrag={async (event) => {
+        const scrollingToBottom = (event.nativeEvent.velocity?.y ?? 0) > 0;
+
+        // Move to store
+        // offset = scrollingToBottom ?
+        //   offset + limit: 
+        //   offset - limit;
+
+        await dispatch(photosThunks.loadLocalPhotosThunk({ limit, offset }));
+      }}
       numColumns={columnsCount}
       onEndReached={() => undefined}
       onEndReachedThreshold={3}
