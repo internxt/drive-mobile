8aa9830ed4cf14641b960516bf86a5553bcf847b
Carlos Sala Samper <carlos15695@hotmail.com>
2022-02-23 11:14:33 +0100

    changed publish version to 1.5.6 (6)

--- src/components/VersionUpdate/index.tsx
+++ src/components/VersionUpdate/index.tsx
@@ -21,7 +21,7 @@ function VersionUpdate(): JSX.Element {
   return (
     <View>
       <Text style={tailwind('text-center text-base text-sm text-gray-50')}>
-        Internxt Drive v{PackageJson.version} (5)
+        Internxt Drive v{PackageJson.version} (6)
       </Text>
     </View>
   );
