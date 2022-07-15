4c65ca52c75d8eae36ac93bcfe1d3edf9f643cf1
Robert <robertogarciadevelopment@gmail.com>
2022-07-15 13:03:00 +0200

    [PHOT-4]: Refactor gallery item component to be functional

--- src/components/GalleryItem/index.tsx
+++ src/components/GalleryItem/index.tsx
@@ -16,42 +16,34 @@ interface GalleryItemProps {
   onLongPress?: (photo: Photo, preview: string | null) => void;
 }
 
-class GalleryItem extends React.PureComponent<GalleryItemProps> {
-  constructor(props: GalleryItemProps) {
-    super(props);
-  }
+const GalleryItem: React.FC<GalleryItemProps> = (props) => {
+  const { onPress, data, size, onLongPress, isSelected } = props;
+  const { resolvedPreview } = data;
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      style={[tailwind('bg-white'), { width: size, height: size }]}
+      onPress={() => onPress && resolvedPreview && onPress(data, resolvedPreview)}
+      onLongPress={() => onLongPress && resolvedPreview && onLongPress(data, resolvedPreview)}
+    >
+      {resolvedPreview && (
+        <FastImage
+          style={tailwind('w-full h-full')}
+          source={{
+            uri: resolvedPreview,
+          }}
+        />
+      )}
 
-  render(): React.ReactNode {
-    const { onPress, data, size, onLongPress, isSelected } = this.props;
-    const { resolvedPreview } = data;
-    return (
-      <TouchableOpacity
-        activeOpacity={0.7}
-        style={[tailwind('bg-white'), { width: size, height: size }]}
-        onPress={() => onPress && resolvedPreview && onPress(data, resolvedPreview)}
-        onLongPress={() => onLongPress && resolvedPreview && onLongPress(data, resolvedPreview)}
-      >
-        {resolvedPreview && (
-          <FastImage
-            style={tailwind('w-full h-full')}
-            source={{
-              uri: resolvedPreview,
-            }}
-          />
-        )}
-
-        {isSelected && (
-          <View
-            style={[
-              tailwind('absolute bg-blue-60 w-5 h-5 bottom-3 right-3 flex justify-center items-center rounded-xl'),
-            ]}
-          >
-            <CheckCircle color={getColor('white')} size={30} />
-          </View>
-        )}
-      </TouchableOpacity>
-    );
-  }
-}
+      {isSelected && (
+        <View
+          style={[tailwind('absolute bg-blue-60 w-5 h-5 bottom-3 right-3 flex justify-center items-center rounded-xl')]}
+        >
+          <CheckCircle color={getColor('white')} size={30} />
+        </View>
+      )}
+    </TouchableOpacity>
+  );
+};
 
 export default GalleryItem;
