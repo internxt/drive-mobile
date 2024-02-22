3add9501bf9fe8081c091ceb5da73e9f23f63e4b
Ramon Candel <ramoncandelsegura@gmail.com>
2024-02-22 13:55:20 +0100

    added doc and imports

--- ios/KyberCrypto.swift
+++ ios/KyberCrypto.swift
@@ -1,14 +1,19 @@
+import Foundation
+import SwiftKyber
+
 class KyberCrypto {
-    // Generate a public-private key pair
-    func generateAsymmetricKeyPair() -> (publicKey: Data, privateKey: Data)? {
-        do {
-            let keyPair = try Kyber.generateKeyPair(keySize: keySize)
-            return (keyPair.publicKey, keyPair.privateKey)
-        } catch {
-            print("Error generating asymmetric keys: \(error)")
-            return nil
-        }
-    }
+  // Generate a public-private key pair for asymmetric encryption
+  // - Returns: A tuple containing the public key and private key as Data, or nil in case of an error.
+  func generateAsymmetricKeyPair() -> (publicKey: Data, privateKey: Data)? {
+      do {
+          let keyPair = try Kyber.generateKeyPair(keySize: keySize)
+          return (keyPair.publicKey, keyPair.privateKey)
+      } catch {
+          print("Error generating asymmetric keys: \(error)")
+          return nil
+      }
+  }
+
 
     // Encrypt a message with a public key
     func encryptMessage(_ message: Data, using publicKey: Data) -> (ciphertext: Data, sharedSecret: Data)? {
