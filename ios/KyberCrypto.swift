3414bb0a37bcddca47a48843eae51ae19f685e4b
Ramon Candel <ramoncandelsegura@gmail.com>
2024-02-29 16:30:10 +0100

    added more doc

--- ios/KyberCrypto.swift
+++ ios/KyberCrypto.swift
@@ -2,20 +2,24 @@ import Foundation
 import SwiftKyber
 
 class KyberCrypto {
-  // Generate a public-private key pair for asymmetric encryption
-  // - Returns: A tuple containing the public key and private key as Data, or nil in case of an error.
-  func generateAsymmetricKeyPair() -> (publicKey: Data, privateKey: Data)? {
-      do {
-          let keyPair = try Kyber.generateKeyPair(keySize: keySize)
-          return (keyPair.publicKey, keyPair.privateKey)
-      } catch {
-          print("Error generating asymmetric keys: \(error)")
-          return nil
-      }
-  }
+    // Generate a public-private key pair for asymmetric encryption
+    // - Returns: A tuple containing the public key and private key as Data, or nil in case of an error.
+    func generateAsymmetricKeyPair() -> (publicKey: Data, privateKey: Data)? {
+        do {
+            let keyPair = try Kyber.generateKeyPair(keySize: keySize)
+            return (keyPair.publicKey, keyPair.privateKey)
+        } catch {
+            print("Error generating asymmetric keys: \(error)")
+            return nil
+        }
+    }
 
 
-    // Encrypt a message with a public key
+    // Encrypt a message using a public key
+    // - Parameters:
+    //   - message: The data to encrypt.
+    //   - publicKey: The public key used for encryption.
+    // - Returns: A tuple containing the ciphertext and the shared secret, or nil in case of an error.
     func encryptMessage(_ message: Data, using publicKey: Data) -> (ciphertext: Data, sharedSecret: Data)? {
         do {
             let encryptionResult = try Kyber.encrypt(publicKey: publicKey, message: message)
