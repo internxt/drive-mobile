7b2513f4d9e552cffc39db31977c2e16c299aad2
Ramon Candel <robertogarciadevelopment@gmail.com>
2024-06-20 15:10:55 +0200

    Improved tests

--- ios/KyberCryptoTests.swift
+++ ios/KyberCryptoTests.swift
@@ -11,9 +11,10 @@ class KyberCryptoTests: XCTestCase {
     func testKeyGeneration() {
         let kyber = KyberCrypto()
         let keyPair = kyber.generateAsymmetricKeyPair()
-        XCTAssertNotNil(keyPair, "Key pair should not be nil")
         XCTAssertEqual(keyPair?.publicKey.count ?? 0 > 0, true, "Public key should be non-empty")
         XCTAssertEqual(keyPair?.privateKey.count ?? 0 > 0, true, "Private key should be non-empty")
+        XCTAssertNotNil(keyPair, "Key pair generation failed unexpectedly")
+        XCTAssertEqual(keyPair?.publicKey.count ?? 0 > 0, true, "Expected a non-empty public key")
     }
 
     func testEncryption() {
@@ -51,6 +52,7 @@ class KyberCryptoTests: XCTestCase {
         kyber.keySize = 999 // Invalid key size
         let keyPair = kyber.generateAsymmetricKeyPair()
         XCTAssertNil(keyPair, "Key generation should fail for invalid key size")
+
     }
 
 }
