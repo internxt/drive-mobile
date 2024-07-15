df4eb5846757e68b69e082f614300badcffa76b1
Ramon Candel <ramoncandelsegura@gmail.com>
2024-07-15 11:40:22 +0200

    Use kyber manager

--- ios/KyberCrypto.swift
+++ ios/KyberCrypto.swift
@@ -64,15 +64,15 @@ class KyberCrypto {
     //   - message: The data to encrypt.
     //   - publicKey: The public key used for encryption.
     // - Returns: A tuple containing the ciphertext and the shared secret, or nil in case of an error.
-    func encryptMessage(_ message: Data, using publicKey: Data) -> (ciphertext: Data, sharedSecret: Data)? {
+      func encryptMessage(_ message: Data, using publicKey: Data) -> (ciphertext: Data, sharedSecret: Data)? {
         do {
-            let encryptionResult = try Kyber.encrypt(publicKey: publicKey, message: message)
-            return (encryptionResult.ciphertext, encryptionResult.sharedSecret)
+            return try KyberEncryptionManager.encryptMessage(message, with: publicKey)
         } catch {
-            print("Error encrypting message: \(KyberError.encryptionFailed)")
+            logError(.encryptionFailed)
             return nil
         }
     }
+
 
     // Decrypt a ciphertext using a private key
     // - Parameters:
@@ -81,10 +81,9 @@ class KyberCrypto {
     // - Returns: A tuple containing the original message and the shared secret, or nil in case of an error.
     func decryptCiphertext(_ ciphertext: Data, using privateKey: Data) -> (message: Data, sharedSecret: Data)? {
         do {
-            let decryptionResult = try Kyber.decrypt(privateKey: privateKey, ciphertext: ciphertext)
-            return (decryptionResult.message, decryptionResult.sharedSecret)
+            return try KyberEncryptionManager.decryptCiphertext(ciphertext, with: privateKey)
         } catch {
-            print("Error decrypting ciphertext: \(KyberError.decryptionFailed)")
+            logError(.decryptionFailed)
             return nil
         }
     }
