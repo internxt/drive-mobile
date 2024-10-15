222872c147d6787c5b5d05a9b3419a0a328a7b3d
Ramon Candel <mameo@MBP-de-Ramon.home>
2024-10-15 09:30:49 +0200

    Fixed metro config

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,39 @@
+// const { getDefaultConfig } = require('@expo/metro-config');
+
+// module.exports = (async () => {
+//   const { resolver, transformer } = await getDefaultConfig(__dirname);
+
+//   return {
+//     transformer: {
+//       ...transformer,
+//       babelTransformerPath: require.resolve('react-native-svg-transformer'),
+//       assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+//     },
+//     resolver: {
+//       ...resolver,
+//       extraNodeModules: require('node-libs-react-native'),
+//       assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+//       sourceExts: [...resolver.sourceExts, 'svg', 'jsx', 'js', 'ts', 'tsx', 'cjs'],
+//     },
+//   };
+// })();
+
 const { getDefaultConfig } = require('@expo/metro-config');
 
-module.exports = (async () => {
-  const { resolver, transformer } = await getDefaultConfig(__dirname);
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname, {
+  isCSSEnabled: false,
+});
 
-  return {
-    transformer: {
-      ...transformer,
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-      assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-    },
-    resolver: {
-      ...resolver,
-      extraNodeModules: require('node-libs-react-native'),
-      assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-      sourceExts: [...resolver.sourceExts, 'svg'],
-    },
-  };
-})();
+module.exports = {
+  ...config,
+  transformer: {
+    ...config.transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+  },
+  resolver: {
+    ...config.resolver,
+    extraNodeModules: require('node-libs-react-native'),
+  },
+};
