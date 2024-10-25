cdc8b46847e2385eb8b89dc393781079b890baeb
Ramon Candel <mameo@MBP-de-Ramon.home>
2024-10-25 11:37:45 +0200

    Fixed Android cursor position bug in TextInput

--- src/components/AppTextInput/index.tsx
+++ src/components/AppTextInput/index.tsx
@@ -1,7 +1,16 @@
 import { isString } from 'lodash';
-import { useState } from 'react';
-import { StyleProp, TextInput, TextInputProps, View, ViewStyle } from 'react-native';
-import { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+import { useRef, useState } from 'react';
+import {
+  NativeSyntheticEvent,
+  Platform,
+  StyleProp,
+  TextInput,
+  TextInputFocusEventData,
+  TextInputProps,
+  TextInputSelectionChangeEventData,
+  View,
+  ViewStyle,
+} from 'react-native';
 import { useTailwind } from 'tailwind-rn';
 import useGetColor from '../../hooks/useColor';
 import AppText from '../AppText';
@@ -19,8 +28,56 @@ const AppTextInput = (props: AppTextInputProps): JSX.Element => {
   const tailwind = useTailwind();
   const getColor = useGetColor();
   const [isFocused, setIsFocused] = useState(false);
+  const [selection, setSelection] = useState<{ start: number; end: number } | undefined>();
+  const localInputRef = useRef<TextInput>(null);
   const editable = props.editable !== false;
   const [status, statusMessage] = props.status || ['idle', ''];
+  const isAndroid = Platform.OS === 'android';
+
+  const handleSelectionChange = (event: NativeSyntheticEvent<TextInputSelectionChangeEventData>) => {
+    if (isAndroid) {
+      const newSelection = event.nativeEvent.selection;
+      setSelection(newSelection);
+    }
+  };
+
+  const handleOnFocusInput = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
+    setIsFocused(true);
+    if (isAndroid && props.value && (!selection || (selection.start === 0 && selection.end === 0))) {
+      const position = props.value.length;
+      setSelection({ start: position, end: position });
+    }
+    props.onFocus?.(e);
+  };
+
+  // Added selection prop handler to resolve Android input cursor position bug
+  const handleChangeText = (newText: string) => {
+    if (isAndroid && selection) {
+      const oldText = props.value || '';
+      let newPosition: number;
+
+      const hasNothingSelected = selection.start === selection.end;
+      if (hasNothingSelected) {
+        newPosition = selection.start + (newText.length - oldText.length);
+        setSelection({ start: newPosition, end: newPosition });
+      } else {
+        const selectionStart = Math.min(selection.start, selection.end);
+        newPosition = selectionStart + newText.length - oldText.slice(selection.start, selection.end).length;
+        setSelection({ start: newPosition, end: newPosition });
+      }
+
+      setTimeout(() => {
+        if (localInputRef.current) {
+          localInputRef.current.setNativeProps({
+            selection: { start: newPosition, end: newPosition },
+          });
+        }
+      }, 0);
+    }
+
+    props.onChangeText?.(newText);
+  };
+
   const renderStatusMessage = () => {
     let template: JSX.Element | undefined = undefined;
 
@@ -47,6 +104,14 @@ const AppTextInput = (props: AppTextInputProps): JSX.Element => {
     return template;
   };
 
+  const inputProps = {
+    ...props,
+    ref: props.inputRef || localInputRef,
+    onChangeText: isAndroid ? handleChangeText : props.onChangeText,
+    onSelectionChange: isAndroid ? handleSelectionChange : props.onSelectionChange,
+    selection: isAndroid ? selection : props.selection,
+  };
+
   return (
     <View style={props.containerStyle}>
       {props.label && <AppText style={tailwind('text-sm mb-1')}>{props.label}</AppText>}
@@ -63,11 +128,13 @@ const AppTextInput = (props: AppTextInputProps): JSX.Element => {
       >
         <TextInput
           placeholderTextColor={getColor('text-gray-30')}
-          {...props}
+          {...inputProps}
           style={[tailwind('flex-1 text-gray-80 py-2 px-4'), props.style]}
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-          ref={props.inputRef}
+          onFocus={handleOnFocusInput}
+          onBlur={(e) => {
+            setIsFocused(false);
+            props.onBlur?.(e);
+          }}
         />
         {props.renderAppend && <View style={tailwind('px-4')}>{props.renderAppend({ isFocused })}</View>}
       </View>
