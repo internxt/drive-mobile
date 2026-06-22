ebd202d8450acfcf41aeebecb2035542433e7d94
Ramon Candel <ramoncandelsegura@gmail.com>
2026-06-22 12:11:18 +0200

    Fixed security modal dispaly on Account screen

--- src/components/modals/SecurityModal/index.tsx
+++ src/components/modals/SecurityModal/index.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
-
-import strings from '../../../../assets/lang/strings';
-import CenterModal from '../CenterModal';
-import AppButton from '../../AppButton';
-import { useTailwind } from 'tailwind-rn';
-import AppText from '../../AppText';
-import { BaseModalProps } from '../../../types/ui';
+import Portal from '@burstware/react-native-portal';
 import { useNavigation } from '@react-navigation/native';
-import { SettingsScreenNavigationProp } from 'src/types/navigation';
+import { useState } from 'react';
+import { View } from 'react-native';
 import AuthenticationForm from 'src/components/forms/AuthenticationForm';
+import { SettingsScreenNavigationProp } from 'src/types/navigation';
+import { useTailwind } from 'tailwind-rn';
+import strings from '../../../../assets/lang/strings';
+import { BaseModalProps } from '../../../types/ui';
+import AppButton from '../../AppButton';
+import AppText from '../../AppText';
+import CenterModal from '../CenterModal';
 
 const SecurityModal = (props: BaseModalProps) => {
   const tailwind = useTailwind();
@@ -30,47 +30,49 @@ const SecurityModal = (props: BaseModalProps) => {
   };
 
   return (
-    <CenterModal
-      isOpen={props.isOpen}
-      onClosed={onClosed}
-      backdropPressToClose={!isLoading}
-      backButtonClose={!isLoading}
-    >
-      <View style={tailwind('p-4')}>
-        <AppText style={tailwind('text-xl mb-1.5')} medium>
-          {strings.modals.Security.title}
-        </AppText>
+    <Portal>
+      <CenterModal
+        isOpen={props.isOpen}
+        onClosed={onClosed}
+        backdropPressToClose={!isLoading}
+        backButtonClose={!isLoading}
+      >
+        <View style={tailwind('p-4')}>
+          <AppText style={tailwind('text-xl mb-1.5')} medium>
+            {strings.modals.Security.title}
+          </AppText>
 
-        <AppText style={tailwind('mb-6 text-sm text-gray-60')}>{strings.modals.Security.advice}</AppText>
+          <AppText style={tailwind('mb-6 text-sm text-gray-60')}>{strings.modals.Security.advice}</AppText>
 
-        <AuthenticationForm
-          onFormLoadingChange={onFormLoadingChanged}
-          onFormSubmitSuccess={onFormSubmitSuccess}
-          renderActionsContainer={({ isDirty, isValid, isLoading, onSubmitButtonPressed }) => (
-            <View style={tailwind('mt-6 flex-row justify-between')}>
-              <AppButton
-                title={strings.buttons.cancel}
-                type="cancel"
-                onPress={onCancelButtonPressed}
-                disabled={isLoading}
-                style={tailwind('flex-1')}
-              />
+          <AuthenticationForm
+            onFormLoadingChange={onFormLoadingChanged}
+            onFormSubmitSuccess={onFormSubmitSuccess}
+            renderActionsContainer={({ isDirty, isValid, isLoading, onSubmitButtonPressed }) => (
+              <View style={tailwind('mt-6 flex-row justify-between')}>
+                <AppButton
+                  title={strings.buttons.cancel}
+                  type="cancel"
+                  onPress={onCancelButtonPressed}
+                  disabled={isLoading}
+                  style={tailwind('flex-1')}
+                />
 
-              <View style={tailwind('px-1')}></View>
+                <View style={tailwind('px-1')}></View>
 
-              <AppButton
-                title={isLoading ? strings.buttons.accessing : strings.buttons.access}
-                type="accept"
-                onPress={onSubmitButtonPressed}
-                loading={isLoading}
-                disabled={!isDirty || !isValid || isLoading}
-                style={tailwind('flex-1')}
-              />
-            </View>
-          )}
-        />
-      </View>
-    </CenterModal>
+                <AppButton
+                  title={isLoading ? strings.buttons.accessing : strings.buttons.access}
+                  type="accept"
+                  onPress={onSubmitButtonPressed}
+                  loading={isLoading}
+                  disabled={!isDirty || !isValid || isLoading}
+                  style={tailwind('flex-1')}
+                />
+              </View>
+            )}
+          />
+        </View>
+      </CenterModal>
+    </Portal>
   );
 };
 
