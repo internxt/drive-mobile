539fdf15b846b43f19dcea074fbe5947984fa361
Ramon Candel <ramoncandelsegura@gmail.com>
2026-09-04 16:17:23 +0200

    Add a mailbox drawer to Mail and migrate React Navigation to v7

--- src/navigation/MailNavigator.tsx
+++ src/navigation/MailNavigator.tsx
@@ -1,15 +1,37 @@
-import { EmailDetailScreen } from 'src/screens/mail/EmailDetailScreen';
-import { MailStackParamList } from '@internxt-mobile/types/navigation';
+import { MailDrawerParamList, MailStackParamList } from '@internxt-mobile/types/navigation';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { EmailDetailScreen } from 'src/screens/mail/EmailDetailScreen';
+
+import { MailContextProvider } from '../contexts/Mail/Mail.context';
 import MailboxListScreen from '../screens/mail/MailboxListScreen';
+import { MAILBOX_ORDER, MailboxId } from '../types/mail';
+import MailDrawerContent from './MailDrawerContent';
 
 const MailStack = createNativeStackNavigator<MailStackParamList>();
+const MailDrawer = createDrawerNavigator<MailDrawerParamList>();
+
+const MailboxDrawerNavigator = () => {
+  return (
+    <MailDrawer.Navigator
+      initialRouteName={MailboxId.Inbox}
+      drawerContent={(props) => <MailDrawerContent {...props} />}
+      screenOptions={{ headerShown: false, drawerType: 'front', drawerStyle: { width: '78%' } }}
+    >
+      {MAILBOX_ORDER.map((mailboxId) => (
+        <MailDrawer.Screen key={mailboxId} name={mailboxId} component={MailboxListScreen} />
+      ))}
+    </MailDrawer.Navigator>
+  );
+};
 
 export const MailNavigator = () => {
   return (
-    <MailStack.Navigator screenOptions={{ headerShown: false }}>
-      <MailStack.Screen name="MailboxList" component={MailboxListScreen} options={{ animation: 'default' }} />
-      <MailStack.Screen name="EmailDetail" component={EmailDetailScreen} />
-    </MailStack.Navigator>
+    <MailContextProvider>
+      <MailStack.Navigator screenOptions={{ headerShown: false }}>
+        <MailStack.Screen name="MailboxDrawer" component={MailboxDrawerNavigator} options={{ animation: 'default' }} />
+        <MailStack.Screen name="EmailDetail" component={EmailDetailScreen} />
+      </MailStack.Navigator>
+    </MailContextProvider>
   );
 };
