028a94926c0eb28685853eb41809506806ff47d0
Ramon Candel <ramoncandelsegura@gmail.com>
2026-09-07 09:24:47 +0200

    Adapted mail navigation drawer to dark mode

--- src/navigation/MailNavigator.tsx
+++ src/navigation/MailNavigator.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { EmailDetailScreen } from 'src/screens/mail/EmailDetailScreen';
 
 import { MailContextProvider } from '../contexts/Mail/Mail.context';
+import useGetColor from '../hooks/useColor';
 import MailboxListScreen from '../screens/mail/MailboxListScreen';
 import { MAILBOX_ORDER, MailboxId } from '../types/mail';
 import MailDrawerContent from './MailDrawerContent';
@@ -12,11 +13,17 @@ const MailStack = createNativeStackNavigator<MailStackParamList>();
 const MailDrawer = createDrawerNavigator<MailDrawerParamList>();
 
 const MailboxDrawerNavigator = () => {
+  const getColor = useGetColor();
+
   return (
     <MailDrawer.Navigator
       initialRouteName={MailboxId.Inbox}
       drawerContent={(props) => <MailDrawerContent {...props} />}
-      screenOptions={{ headerShown: false, drawerType: 'front', drawerStyle: { width: '78%' } }}
+      screenOptions={{
+        headerShown: false,
+        drawerType: 'front',
+        drawerStyle: { width: '78%', backgroundColor: getColor('bg-surface') },
+      }}
     >
       {MAILBOX_ORDER.map((mailboxId) => (
         <MailDrawer.Screen key={mailboxId} name={mailboxId} component={MailboxListScreen} />
